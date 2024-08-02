44fef26b9297d6ee2a4f828fcb88fc04c29e3775
ConceicaoLarissa <conceicao.fatec.ads@gmail.com>
2024-08-02 13:46:45 -0300

    Criação do Projeto de gerenciamento para arquivos

---
+++ Gerenciamento-de-Arquivos/src/main/java/com/gerencimento/Gerenciamento/de/Arquivos/GerenciamentoDeArquivosApplication.java
@@ -0,0 +1,13 @@
+package com.gerencimento.Gerenciamento.de.Arquivos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeArquivosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeArquivosApplication.class, args);
+	}
+
+}
